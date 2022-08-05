c7099e9544393711695aee2ee32924a9fbd4764a
Tianjie Shen <tianjieshen@microsoft.com>
2022-08-05 16:17:28 -0700

    update main body

--- EventHubTrigger/Function1Helpers.cs
+++ EventHubTrigger/Function1Helpers.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Azure.Data.Tables;
 using EventHubTrigger.Entity;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
@@ -50,6 +49,7 @@ namespace EventHubTrigger
             var tokenProvider = new AzureServiceTokenProvider();
             var token = await tokenProvider.GetAccessTokenAsync("https://rp.core.security.dev1.azure.com/") ??
                         string.Empty;
+
             foreach (EventData eventData in events)
             {
                 try
@@ -58,19 +58,52 @@ namespace EventHubTrigger
                     log.LogInformation("C# Event Hub trigger function processed a message: {EventDataEventBody}", eventData.EventBody);
                     _telemetryClient.TrackTrace($"C# event hub message properties is: {eventData.Properties}");
                     _telemetryClient.TrackTrace($"C# Event Hub trigger function processed a message: {eventData.EventBody}", SeverityLevel.Information);
-                    HttpClient client = new HttpClient();
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                    var resp = await client.GetStringAsync(URI);
-                    var results = JsonConvert.DeserializeObject<List<InternalPricingConfig>>(resp);
-                    foreach (var r in results)
+
+                    var e = JsonConvert.DeserializeObject<Event>(eventData.EventBody.ToString());
+                    _telemetryClient.TrackTrace($"event body is {e}");
+                    var id = e.Id.ToString();
+                    var subId = id.Split("/")[1];
+                    if (!e.StandardBundles.Contains("AppServices"))
                     {
-                        _telemetryClient.TrackTrace($"response string from api is: {r.SubscriptionId}");
-                        var m = new Mapping(Guid.NewGuid().ToString(), r.SubscriptionId.ToString(), r.TenantId.ToString());
-                        var createdItem = await container.UpsertItemAsync<Mapping>(
-                            item: m,
-                            partitionKey: new PartitionKey(r.SubscriptionId.ToString())
-                        );
-                        
+                        _telemetryClient.TrackTrace("Event hub notification doesn't contain app services, need to remove from db!");
+                        // Downgrade path
+                        var parameterizedQuery = new QueryDefinition(
+                            query: "SELECT * FROM items p WHERE p.subscriptionId = @partitionKey"
+                        ).WithParameter("@partitionKey", subId);
+                        var feed = container.GetItemQueryIterator<Mapping>(
+                            queryDefinition: parameterizedQuery);
+                        _telemetryClient.TrackTrace($"Feed result is {feed}");
+                        while (feed.HasMoreResults)
+                        {
+                            FeedResponse<Mapping> cur = await feed.ReadNextAsync();
+                            foreach(Mapping ma in cur)
+                            {
+                                ItemResponse<Mapping> response = await container.DeleteItemAsync<Mapping>(ma.id, new PartitionKey(ma.subscriptionId));
+                                _telemetryClient.TrackTrace($"Sucessfully delete item with subscription id: {ma.subscriptionId}");
+                            }
+                        };
+                    }
+                    else
+                    {
+                        // Upgrade path insert
+                        _telemetryClient.TrackTrace($"Event hub notification contains the app service, insert the mapping record!");
+                        HttpClient client = new HttpClient();
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                        var resp = await client.GetStringAsync(URI);
+                        var results = JsonConvert.DeserializeObject<List<InternalPricingConfig>>(resp);
+                        foreach (var r in results)
+                        {
+                            if (r.SubscriptionId.Equals(subId))
+                            {
+                                _telemetryClient.TrackTrace($"Find the record: {r}");
+                                var m = new Mapping(Guid.NewGuid().ToString(), r.SubscriptionId.ToString(), r.TenantId.ToString());
+                                var createdItem = await container.UpsertItemAsync(
+                                    item: m,
+                                    partitionKey: new PartitionKey(r.SubscriptionId.ToString())
+                                );
+                            }
+                            
+                        }
                     }
                     await Task.Yield();
                 }
